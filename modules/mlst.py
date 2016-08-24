1cc87ce3670b2978e61d69cc7494a77b9b07d3d9
miguelpmachado <mpmachado@fc.ul.pt>
2016-08-24 11:20:01 +0100

    Change MLST scheme determination, check input and output directory, update combine_reports.py, control for species name
    Check whether input and output directory are the same (fix delete folder
    when both directories are the same).
    Change how MLST scheme is determined (fix species assignment issue).
    Update combine_reports.py for new MLST scheme determination.)
    Control for species name parsing (requires something like Streptococcus
    agalactiae).

--- modules/mlst.py
+++ modules/mlst.py
@@ -5,33 +5,53 @@ from functools import partial
 mlst_timer = partial(utils.timer, name='MLST')
 
 
+def getScheme(species):
+	command = ['which', 'mlst']
+	run_successfully, stdout, stderr = utils.runCommandPopenCommunicate(command, False, None)
+	mlst_folder = stdout.splitlines()[0]
+	mlst_db_path = os.path.join(os.path.dirname(os.path.dirname(mlst_folder)), 'db', 'species_scheme_map.tab')
+
+	species = species.lower().split(' ')
+
+	scheme = 'unknown'
+
+	with open(mlst_db_path, 'rtU') as reader:
+		pass
+		for line in reader:
+			line = line.splitlines()[0]
+			if len(line) > 0:
+				if not line.startswith('#'):
+					line = line.lower().split('\t')
+					if line[0] == species[0] and line[1] == species[1]:
+						scheme = line[2]
+
+	return scheme
+
+
 @mlst_timer
-def runMlst(contigs, species, outdir):
+def runMlst(contigs, scheme, outdir):
 	pass_qc = False
 	failing = {}
 	failing['sample'] = False
-
-	species = species.lower().split(' ')
-	species = species[0][0] + species[1]
 
 	command = ['mlst', contigs]
 
 	run_successfully, stdout, stderr = utils.runCommandPopenCommunicate(command, False, None)
 
 	if run_successfully:
-		species_mlst = stdout.splitlines()[0].split('\t')[1].split('_')[0]
+		scheme_mlst = stdout.splitlines()[0].split('\t')[1].split('_')[0]
 		st = stdout.splitlines()[0].split('\t')[2]
 
-		report = 'MLST found ST ' + str(st) + ' from species ' + species_mlst
+		report = 'MLST found ST ' + str(st) + ' from scheme ' + scheme_mlst
 		print report
 		with open(os.path.join(outdir, 'mlst_report.txt'), 'wt') as writer:
-			writer.write('#species' + '\n' + species_mlst + '\n' + '#ST' + '\n' + st + '\n')
+			writer.write('#scheme' + '\n' + scheme_mlst + '\n' + '#ST' + '\n' + st + '\n')
 			writer.flush()
 
-		if species_mlst == species:
+		if scheme_mlst == scheme:
 			pass_qc = True
 		else:
-			failing['sample'] = 'Species found (' + species_mlst + ') and provided (' + species + ') are not the same'
+			failing['sample'] = 'MLST scheme found (' + scheme_mlst + ') and provided (' + scheme + ') are not the same'
 			print failing['sample']
 	else:
 		failing['sample'] = 'Did not run;'
