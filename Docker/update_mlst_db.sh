c1dc7d5c7087792458cb24ee9382657d22b678d7
cimendes <iines.mendes@gmail.com>
2017-06-22 13:46:49 +0100

    - Fix bug

--- Docker/update_mlst_db.sh
+++ Docker/update_mlst_db.sh
@@ -2,8 +2,12 @@
 
 #script to update the mlst database
 
+currentDir=$(pwd)
+
 cd /NGStools/mlst/scripts/
 ./mlst-download_pub_mlst | bash
 mv ../db/pubmlst ../db/pubmlst.old
 mv ./pubmlst ../db/
 ./mlst-make_blast_db
+
+cd $currentDir
