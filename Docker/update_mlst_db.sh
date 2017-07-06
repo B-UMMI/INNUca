40641225c96e1d1692006103931551b3c064a13b
cimendes <iines.mendes@gmail.com>
2017-07-06 10:45:00 +0100

    - reverted mlst changes in docker

--- Docker/update_mlst_db.sh
+++ Docker/update_mlst_db.sh
@@ -2,8 +2,8 @@
 
 #script to update the mlst database
 
-/tmp/mlst/scripts/mlst-download_pub_mlst | bash
-rm -r /tmp/mlst/db/pubmlst/
-mv /tmp/mlst/scripts/pubmlst /tmp/mlst/db/
-/tmp/mlst/scripts/mlst-make_blast_db
+/NGStools/mlst/scripts/mlst-download_pub_mlst | bash
+rm -r /NGStools/mlst/db/pubmlst/
+mv /NGStools/mlst/scripts/pubmlst /NGStools/mlst/db/
+/NGStools/mlst/scripts/mlst-make_blast_db
 
